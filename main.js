b3ef5589e3b17640df5fc0a66f0568a9cfec65a4
kaimet <114295072+kaimet@users.noreply.github.com>
2024-05-25 22:27:12 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const ringColor2 = 0x888888;
 
 const segmentCount = 150; // visible segments
 const segmentDistance = 2; // rings density
-const tunnelLength = 1000; // segments
+let tunnelLength = 1000; // segments
 const tunnelRad = 5;
 const wigliness = 1.2;
 const turnForce = 0.05;
@@ -15,10 +15,11 @@ const rollSpeed = 0.0008;
 const rollDamp = 0.98;
 const pitchSpeed = 0.00007;
 const pitchDamp = 0.997;
-const movementSpeed = 0.3;
+
+const skills = [0.2, 0.3, 0.4];
+let movementSpeed = skills[1];
 
 
-document.body.style.cursor = 'none';
 
 import * as THREE from 'three';
 const scene = new THREE.Scene();
@@ -45,11 +46,100 @@ scoreElement.style.color = 'white';
 scoreElement.style.fontSize = '24px';
 document.body.appendChild(scoreElement);
 
+let skillElement = document.createElement('div');
+skillElement.style.position = 'absolute';
+skillElement.style.top = '40px';
+skillElement.style.left = '10px';
+skillElement.style.color = 'white';
+skillElement.style.fontSize = '24px';
+document.body.appendChild(skillElement);
+
+document.body.style.cursor = 'none';
+
+
+const names = [
+  "Amelia", "Emma", "Sophia", "Emily", "Violet", "Layla", "Scarlett", "Abigail", "Eleanor", "Olivia"
+];
+
+const seeds = names;
+
+/*
+usage of random generator:
+var seed = cyrb128("apples");
+var rnd = mulberry32(seed[0]);
+var x = rnd(); // in a range [0; 1]
+*/
+
+// Calling cyrb128 will produce a 128-bit hash value from a string which can be used to seed a PRNG. 
+function cyrb128(str) {
+    let h1 = 1779033703, h2 = 3144134277,
+        h3 = 1013904242, h4 = 2773480762;
+    for (let i = 0, k; i < str.length; i++) {
+        k = str.charCodeAt(i);
+        h1 = h2 ^ Math.imul(h1 ^ k, 597399067);
+        h2 = h3 ^ Math.imul(h2 ^ k, 2869860233);
+        h3 = h4 ^ Math.imul(h3 ^ k, 951274213);
+        h4 = h1 ^ Math.imul(h4 ^ k, 2716044179);
+    }
+    h1 = Math.imul(h3 ^ (h1 >>> 18), 597399067);
+    h2 = Math.imul(h4 ^ (h2 >>> 22), 2869860233);
+    h3 = Math.imul(h1 ^ (h3 >>> 17), 951274213);
+    h4 = Math.imul(h2 ^ (h4 >>> 19), 2716044179);
+    return [(h1^h2^h3^h4)>>>0, (h2^h1)>>>0, (h3^h1)>>>0, (h4^h1)>>>0];
+}
+
+// Mulberry32 is a simple generator with a 32-bit state, but is extremely fast and has good quality randomness
+function mulberry32(a) {
+    return function() {
+      var t = a += 0x6D2B79F5;
+      t = Math.imul(t ^ t >>> 15, t | 1);
+      t ^= t + Math.imul(t ^ t >>> 7, t | 61);
+      return ((t ^ t >>> 14) >>> 0) / 4294967296;
+    }
+}
+
+let rnd = Math.random; 
+
+
+function startNewGame(seedIndex) {
+	if (seedIndex !== undefined) { 
+    rnd = mulberry32(cyrb128(seeds[seedIndex])[0]);
+		tunnelLength = 150;
+  } else { 
+    rnd = Math.random;
+		tunnelLength = 1000;
+  }
+	
+  score = 0;
+  scoreElement.textContent = `Score: ${score}`;
+  camera.position.set(0, 0, 150 * movementSpeed);
+  cameraQuaternion.set(0, 0, 0, 1); // Reset to initial orientation
+  dRoll = 0;
+  dPitch = 0;
+  roll = 0;
+  pitch = 0;
+	curPosition = new THREE.Vector3(0, 0, 0);
+	curDirection = new THREE.Vector3(0, 0, -1);
+	axis = new THREE.Vector3(rnd(), rnd(), rnd()).normalize();
+	ind = 0;
+  curRingIndex = 0;
+	nSegments = 0;
+	scene.background = new THREE.Color(backgroundColor);
+
+  // Clear the existing tunnel
+  while (scene.children.length > 0) {
+    scene.remove(scene.children[0]);
+  }
+
+  buildTunnel();
+}
+
+
 
 document.addEventListener('keydown', function(event) {
     switch (event.key) {
         case 'ArrowUp':
-            //pitch = 1;
+            //pitch = 1; // not used, just 3 keys for controls
             break;
         case 'ArrowDown':
             pitch = 1;
@@ -59,6 +149,57 @@ document.addEventListener('keydown', function(event) {
             break;
         case 'ArrowRight':
             roll = 1;
+            break;
+						
+						
+				case ' ':
+        case 'Enter':
+            startNewGame();
+            break;
+				
+				// predifined tunnels
+        case '1':
+            startNewGame(0);
+            break;
+        case '2':
+            startNewGame(1);
+            break;
+        case '3':
+            startNewGame(2);
+            break;
+        case '4':
+            startNewGame(3);
+            break;
+        case '5':
+            startNewGame(4);
+            break;
+        case '6':
+            startNewGame(5);
+            break;
+        case '7':
+            startNewGame(6);
+            break;
+        case '8':
+            startNewGame(7);
+            break;
+        case '9':
+            startNewGame(8);
+            break;
+        case '0':
+            startNewGame(9);
+            break;
+						
+				case 'q':
+						movementSpeed = skills[0];
+						skillElement.textContent = `Slow`;
+            break;
+				case 'w':
+						movementSpeed = skills[1];
+						skillElement.textContent = ``;
+            break;
+				case 'e':
+						movementSpeed = skills[2];
+						skillElement.textContent = `Fast`;
             break;
     }
 });
@@ -145,11 +286,11 @@ function addSegment() {
 		curPosition.add(curDirection.clone().multiplyScalar(segmentDistance));
 
 		// Slightly adjust the axis of rotation
-		let axisAdjustment = new THREE.Vector3(
-				(Math.random() - 0.5) * wigliness,
-				(Math.random() - 0.5) * wigliness,
-				(Math.random() - 0.5) * wigliness
-		);
+  let axisAdjustment = new THREE.Vector3(
+    (rnd() - 0.5) * wigliness,
+    (rnd() - 0.5) * wigliness,
+    (rnd() - 0.5) * wigliness
+  );
 		axis.add(axisAdjustment).normalize();
 		// Ensure the axis is orthogonal to the current direction
 		axis.projectOnPlane(curDirection).normalize();
@@ -160,7 +301,7 @@ function addSegment() {
 }
 
 
-function buildTunnel(segmentCount = 250, segmentDistance = 2) {
+function buildTunnel() {
 		for (let i = 0; i < segmentCount; i++) {
 				addSegment();
     }
@@ -234,7 +375,7 @@ function setLight() {
 
 
 setLight();
-buildTunnel(segmentCount, segmentDistance);
+buildTunnel();
 
 gameLoop();
 function gameLoop() {
